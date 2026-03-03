28dbf94fe991777ed26bbf3c952bf378c318a37d
NikhilNorth <ns4291@srmist.edu.in>
2026-03-03 09:43:52 +0530

    Update UseCase1PalindromeCheckerApp.java

--- UseCase1PalindromeCheckerApp.java
+++ UseCase1PalindromeCheckerApp.java
@@ -2,11 +2,9 @@ public class UseCase1PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("        Palindrome Checker App");
-        System.out.println("=================================");
+        System.out.println("Palindrome Checker App");
         System.out.println("Version : 1.0");
-        System.out.println("UC1 : Application Entry & Welcome Message");
-        System.out.println("=================================");
+        System.out.println("UC1 : Welcome to Nikhil's Palindrome Checker ");
+        System.out.println("UC1 done");
     }
 }
