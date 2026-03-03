e0421854f8639064212defc6c743aff6bc4025f4
NikhilNorth <ns4291@srmist.edu.in>
2026-03-03 23:11:07 +0530

    Update PalindromeCheckerApp.java

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,27 +1,53 @@
-import java.util.Scanner;
+interface PalindromeStrategy {
+    boolean isPalindrome(String text);
+}
 
-public class PalindromeCheckerApp {
+class ReversePalindromeStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String text) {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equals(reversed);
+    }
+}
 
-    public static boolean isPalindrome(String input) {
-        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+class LoopPalindromeStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String text) {
+        int left = 0;
+        int right = text.length() - 1;
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
 
-        String reversed = new StringBuilder(cleaned).reverse().toString();
+class PalindromeService {
+    private PalindromeStrategy strategy;
 
-        return cleaned.equals(reversed);
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
     }
 
+    public void setStrategy(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean check(String text) {
+        return strategy.isPalindrome(text);
+    }
+}
+
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        PalindromeService service = new PalindromeService(new ReversePalindromeStrategy());
 
-        System.out.print("Enter a string: ");
-        String userInput = scanner.nextLine();
+        String word = "racecar";
+        System.out.println(service.check(word));
 
-        if (isPalindrome(userInput)) {
-            System.out.println("It is a palindrome.");
-        } else {
-            System.out.println("It is not a palindrome.");
-        }
-
-        scanner.close();
+        service.setStrategy(new LoopPalindromeStrategy());
+        System.out.println(service.check(word));
     }
 }
